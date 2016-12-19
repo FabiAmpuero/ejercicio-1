7d32c2563e20ebccde437b4d5d6ab4c2e9d1343c
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2016-12-19 16:59:27 -0500

    ejercicio 1 terminado

---
+++ main.js
@@ -0,0 +1,8 @@
+var num1, num2, resultado;
+
+num1 = prompt("Por favor, ingresa un numero");
+num2 = prompt("Por favor, ingresa otro numero");
+
+resultado = parseFloat(num1) + parseFloat(num2);
+
+alert(resultado);
